13126fd021432142665c7220afd901dcc811c4cf
Burak Kumaş <77480694+Burak-Kumas@users.noreply.github.com>
2022-02-07 01:27:54 +0300

    update

--- v1/src/models/Users.js
+++ v1/src/models/Users.js
@@ -3,10 +3,17 @@ const UserSchema = new mongoose.Schema(
   {
     name: String,
     password: String,
-    email: String,
+    email: {
+      type: String,
+      unique: true,
+    },
     profile_image: String,
   },
   { versionKey: false, timestamps: true }
 );
+UserSchema.path("email").validate(async (email) => {
+  const emailCount = await mongoose.models.user.countDocuments({ email });
+  return !emailCount;
+}, "Email already exists");
 
 module.exports = mongoose.model("user", UserSchema);
--- v1/src/validations/Users.js
+++ v1/src/validations/Users.js
@@ -1,17 +1,16 @@
 const Joi = require("joi");
 
 const createValidation = Joi.object({
-  full_name: Joi.string().required().min(3),
-  password: Joi.string().required().min(8),
-  email: Joi.string().email().required(),
+  name: Joi.string().required().min(3).trim(),
+  password: Joi.string().required().min(8).trim(),
+  email: Joi.string().email().required().trim(),
 });
 const loginValidation = Joi.object({
-
-  password: Joi.string().required().min(8),
-  email: Joi.string().email().required(),
+  password: Joi.string().required().min(8).trim(),
+  email: Joi.string().email().required().trim(),
 });
 
 module.exports = {
   createValidation,
-  loginValidation
+  loginValidation,
 };
