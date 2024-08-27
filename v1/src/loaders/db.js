f36f3c97eb45b7be2f31733a3b3828718221196c
Burak Kumaş <brkkumas@gmail.com>
2024-08-27 19:50:02 +0300

    update package

--- v1/src/loaders/db.js
+++ v1/src/loaders/db.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
-const db = mongoose.connection;
-
-db.once("open", () => {});
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
-  await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    const connectionString = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+    console.log("Connecting to MongoDB with:", connectionString); // Bağlantı dizisini kontrol etmek için log ekledik
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully.");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 };
-
+console.log(`Host: ${process.env.DB_HOST}`);
+console.log(`Port: ${process.env.DB_PORT}`);
+console.log(`Database Name: ${process.env.DB_NAME}`);
 module.exports = {
   connectDB,
 };
